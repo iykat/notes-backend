4de3c1843ddb4def6a10e6264030e3dbb82fe6b5
iykat <princeiykat@gmail.com>
2024-01-12 01:42:26 +0000

    Mount router on app and add express middlware to parse requests

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('hello world')
+require('dotenv').config()
+import express from 'express'
+import config from 'config'
+import connectToDb from './utils/connectToDb'
+import log from './utils/logger'
+import router from './routes'
+
+const app = express()
+app.use(express.json())
+
+app.use(router)
+
+const port = config.get('port')
+
+app.listen(port, () => {
+  log.info(`App started at http://localhost:${port}`)
+
+  connectToDb();
+}) 
