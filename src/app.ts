c911f09f836e91fdf2ebbfb02aa3fef39ffb1714
iykat <princeiykat@gmail.com>
2024-01-14 11:17:20 +0000

    Fix cors error

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,19 @@ import config from 'config'
 import connectToDb from './utils/connectToDb'
 import log from './utils/logger'
 import router from './routes'
+import cors from 'cors'
+
+
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
 
 const app = express()
 app.use(express.json())
+
+app.use(cors(corsOptions))
 
 app.use(router)
 
